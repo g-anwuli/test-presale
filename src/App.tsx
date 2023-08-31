c1c89ca5ae9d9f0b75cc4d5bef0021e57d113557
Godswill <godswillanwuli@gmail.com>
2023-08-31 16:08:45 +0100

    rounding up presale

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Logo from "./assets/LogoIcon";
 import {
   contract,
+  contractMap,
   getEthPrice,
   getPresaleAllocation,
   getPresaleState,
@@ -10,9 +11,21 @@ import {
 import ToggleMode from "./components/ToogleMode";
 import ProgressIcon from "./assets/ProgressIcon";
 import { Toaster } from "react-hot-toast";
-// import Button from "./components/button";
 import CopyContainer from "./components/CopyContainer";
 import { ethers } from "ethers";
+
+const linkMap = {
+  "56": "https://www.datawallet.com/crypto/add-bnb-chain-to-metamask",
+  "42161": "https://www.datawallet.com/crypto/add-arbitrum-to-metamask",
+  "11155111": "https://www.datawallet.com/crypto/add-sepolia-to-metamask",
+} as Record<string, string>;
+
+const guideMap = {
+  "1": "Buy ETH and send ETH to your wallet",
+  "56": "Buy BNB and send BNB to your wallet",
+  "42161": "Buy ARB or send ARB to your wallet",
+  "11155111": "Head to Sepolia Faucet and get Sepolia eth",
+} as Record<string, string>;
 
 const App = () => {
   const cachwMode = JSON.parse(
@@ -25,13 +38,18 @@ const App = () => {
     eth: 0,
     total: 0,
   });
+  const [chain, setChain] = useState("11155111");
+
+  const changeChain = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setChain(e.target.value);
+  };
 
   useEffect(() => {
     const bal = async () => {
-      const res1 = await getPresaleAllocation();
-      const res2 = await getPresaleState();
-      const res3 = await getTotalSupply();
-      const res4 = await getEthPrice();
+      const res1 = await getPresaleAllocation(chain);
+      const res2 = await getPresaleState(chain);
+      const res3 = await getTotalSupply(chain);
+      const res4 = await getEthPrice(chain);
       const total = parseInt(res3 || "0") * 0.2;
       const balance = parseInt(res1 || "0");
       const eth = parseFloat(res4 || "0");
@@ -55,7 +73,7 @@ const App = () => {
     };
 
     ar();
-    contract.on("BuyTokens", async (tokensToBuy: ethers.BigNumber) => {
+    contract(chain).on("BuyTokens", async (tokensToBuy: ethers.BigNumber) => {
       // This will be called whenever the BuyTokens event is emitted
       const updatedAllocation = Number(
         ethers.utils.formatUnits(tokensToBuy, 18)
@@ -64,7 +82,7 @@ const App = () => {
       setData((data) => ({ ...data, balance: updatedAllocation }));
       await bal();
     });
-  }, []);
+  }, [chain]);
 
   const per = Math.round(
     ((data.total - data.balance) / (data.total || 1)) * 100
@@ -83,12 +101,49 @@ const App = () => {
           <Logo />
         </div>
       </div>
-      <div className="lg:grid lg:grid-cols-2 gap-14 xl:gap-20 items-center w-full px-[4%] py-[50px] lg:py-[100px]  text-zinc-900 dark:text-white">
-        <div>
-          <div className="text-zinc-800 dark:text-white text-[36px] xs:text-[40px] sm:text-[50px] xl:text-[70px] font-bold leading-[1.4]">
-            SpunkySDX is a Revolutionary asset tracking platform
+      <div className="lg:grid lg:grid-cols-2 gap-14 xl:gap-20 items-center w-full px-[4%] py-[50px] lg:py-[100px]">
+        <div className="text-neutral-700 dark:text-neutral-100 tracking-tight leading-[1.5]">
+          <div className="bg-primary/50 p-5 rounded-xl">
+            <div className="text-[24px] sm:text-[28px]  md:text-[32px] font-semibold mb-5">
+              Guide to Presale
+            </div>
+            <div className="text-[16px] sm:text-[18px] md:text-[20px] space-y-[1em]">
+              <div>{guideMap[chain]}</div>
+              {chain !== "1" && (
+                <div>
+                  Add Sepolia testnet to MetaMask{" "}
+                  <a
+                    className="text-fuchsia-600 dark:text-fuchsia-300 bg-neutral-700 py-1 px-1.5 rounded text-[0.7em] font-medium whitespace-nowrap"
+                    href={linkMap[chain]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn more
+                  </a>
+                </div>
+              )}
+              <div>
+                Send some ether to the contract address on the presale page. Say
+                0.1eth
+              </div>
+              <div>
+                Copy contract address and add it to the token section in
+                MetaMask{" "}
+                <a
+                  className="text-fuchsia-600 dark:text-fuchsia-300 bg-neutral-700 py-1 px-1.5 rounded text-[0.7em] font-medium whitespace-nowrap"
+                  href="https://consensys.net/blog/metamask/how-to-add-your-custom-tokens-in-metamask/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="text-neutral-700 dark:text-white text-[16px] xs:text-[20px]  sm:text-[24px] font-normal leading-[35px] tracking-tight my-10">
+          {/* <div className="text-zinc-800 dark:text-white text-[36px] xs:text-[40px] sm:text-[50px] xl:text-[70px] font-bold leading-[1.4]">
+            SpunkySDX is a Revolutionary asset tracking platform
+          </div> */}
+          <div className="text-[16px] sm:text-[18px] md:text-[20px] my-10">
             SpunkySDX leverages advanced AI technology to actively track and
             monitor stolen asset on the EVM chain.
           </div>
@@ -97,7 +152,7 @@ const App = () => {
           </Button> */}
         </div>
         <div className="relative my-10 lg:my-0 mx-auto lg:ml-auto max-w-[530px] w-full">
-          <div className="h-full w-full rounded-3xl xs:rounded-[40px] box-gradient relative z-[1] py-6 sm:py-8 px-[5%]">
+          <div className="h-full w-full rounded-3xl xs:rounded-[40px] box-gradient relative z-[1] py-6 sm:py-8 px-[5%] text-zinc-900 dark:text-neutral-100">
             <div className="justify-center items-center xs:gap-2 xl:gap-[25px] flex font-bold text-[20px] xs:text-[23px] sm:text-[28px]">
               <div className="">PRE-SALE</div>
               <div className="scale-[0.50] xs:scale-[0.70] xl:scale-[0.77] hidden xs:inline-block">
@@ -125,7 +180,18 @@ const App = () => {
                 <ProgressIcon className="transition-all rocket" />
               </div>
             </div>
-            <div className="my-12 xs:my-16 flex justify-between text-[18px] xs:text-[20px] sm:text-[24px] font-bold">
+            <select
+              className="outline-none w-full h-[30px] xs:h-[37px] bg-transparent rounded-[50px] border-primary border px-4 mt-6 xs:mt-8"
+              value={chain}
+              onChange={changeChain}
+            >
+              <option value="11155111">Sepolia</option>
+              <option value="1">Ethereum</option>
+              <option value="42161">Arbitrum</option>
+              <option value="56">Binance Smart Chain</option>
+            </select>
+
+            <div className="mt-8 mb-10 xs:mb-12 flex justify-between text-[18px] xs:text-[20px] sm:text-[24px] font-bold">
               <div>
                 <div className="font-normal mb-2 text-[15px] xs:text-[16px] sm:text-[17px]">
                   Total Raised
@@ -133,6 +199,7 @@ const App = () => {
                 <div>${usdtRaised}</div>
               </div>
             </div>
+
             {data.isPresale && (
               <div className="text-[20px] sm:text-[22px] font-medium mb-2 flex items-center gap-4">
                 Contract Address{" "}
@@ -142,13 +209,11 @@ const App = () => {
               </div>
             )}
             <div
-              className={`text-center text-[17px] xs:text-[18px] sm:text-[20px] text-primary mb-5 truncate  p-4 rounded-lg ${
+              className={`text-center text-[17px] xs:text-[18px] sm:text-[20px] text-primary mb-3 truncate  p-4 rounded-lg ${
                 mode ? "bg-bg_purple/40" : "bg-bg_purple/10"
               }`}
             >
-              {data.isPresale
-                ? "0xFE68Dc4F68112cB08983ec6E041bBf6370F3F39b"
-                : "Presale has ENDED"}
+              {data.isPresale ? contractMap[chain] : "Presale has ENDED"}
             </div>
           </div>
           <div className="purple-ball absolute -top-[80px] -left-[30px] hidden lg:inline-block" />
--- src/utils/spunkyContractCall.ts
+++ src/utils/spunkyContractCall.ts
@@ -5,41 +5,45 @@ export const provider = new ethers.providers.JsonRpcProvider(
   "https://sepolia.infura.io/v3/063a60c10ac741779bd263e121f5148f"
 );
 
-const CONTRACT_ADDRESS = "0x0AE04f70b2Fab115b51d7Ea74f484B9cf301Ba13";
+export const contractMap = {
+  "1": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+  "56": "0x55d398326f99059fF775485246999027B3197955",
+  "42161": "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+  "11155111": "0x0AE04f70b2Fab115b51d7Ea74f484B9cf301Ba13",
+} as Record<string, string>;
 
-export const contract = new ethers.Contract(
-  CONTRACT_ADDRESS,
-  SPUNKYABI,
-  provider
-);
+// const CONTRACT_ADDRESS = "0x0AE04f70b2Fab115b51d7Ea74f484B9cf301Ba13";
+
+export const contract = (chain = "11155111") =>
+  new ethers.Contract(contractMap[chain], SPUNKYABI, provider);
 
 // console.log(contract);
 
-export const getPresaleAllocation = async () => {
+export const getPresaleAllocation = async (chain: string) => {
   if (!provider) return;
-  const transaction = await contract.getPresaleAllocation();
+  const transaction = await contract(chain).getPresaleAllocation();
   const data = await transaction;
   const result = ethers.utils.formatUnits(data, 18);
   return result;
 };
 
-export const getTotalSupply = async () => {
+export const getTotalSupply = async (chain: string) => {
   if (!provider) return;
-  const transaction = await contract.getSupply();
+  const transaction = await contract(chain).getSupply();
   const data = await transaction;
   const result = ethers.utils.formatUnits(data, 18);
   return result;
 };
 
-export const getPresaleState = async () => {
+export const getPresaleState = async (chain: string) => {
   if (!provider) return;
-  const transaction = await contract.getPresaleState();
+  const transaction = await contract(chain).getPresaleState();
   return transaction;
 };
 
-export const getEthPrice = async () => {
+export const getEthPrice = async (chain: string) => {
   if (!provider) return;
-  const transaction = await contract.getETHPrice();
+  const transaction = await contract(chain).getETHPrice();
   const data = await transaction;
   const result = ethers.utils.formatUnits(data, 18);
   return result;
